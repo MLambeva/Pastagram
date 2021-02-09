2463b00ded0dea998aead2161cb6e9ed4628980c
Monika Lambeva <moni4kan@outlook.com>
2021-02-09 14:16:45 +0200

    Removed firebase db dependency in populate

--- js/images.js
+++ js/images.js
@@ -1,8 +1,6 @@
 "use strict";
 
 (() => {
-    const db = firebase.database();
-    const pastasDB = db.ref('/pastas');
     const pastaContainer = document.getElementById('pastas');
     const myPastaContainer = document.getElementById('my-pastas');
     const favPastaContainer = document.getElementById('fav-pastas');
@@ -24,42 +22,6 @@
         }
     });
 
-    if (uploadPasta) {
-        uploadPasta.addEventListener('submit', event => {
-            const URL = document.getElementById('image-URL');
-            const name = document.getElementById('image-name');
-
-            if (URL.value && name.value) {
-                pasta.post(URL.value, name.value);
-            }
-            URL.value = '';
-            name.value = '';
-
-            event.preventDefault();
-        });
-    }
-
-    const post = data => {
-        const post = data.val();
-        if (myPastaContainer) {
-            return `<div class="img-container">
-                        <a href="${post.imageURL}"><img src="${post.imageURL}" class="pasta-image" /></a>
-                    </div>
-                    <label class="image-name">${post.imageName}</label>
-                    <div class="delete-pasta">
-                        <button class="delete-button" data-id="${data.key}">Del-Eat Pasta</button>
-                    </div>`;
-        }
-        else {
-            return `<div class="img-container">
-                        <a href="${post.imageURL}"><img src="${post.imageURL}" class="pasta-image" /></a>
-                    </div>                    
-                    <div class="fav-pasta">
-                        <label class="image-name">${post.imageName}</label>
-                    </div>`;
-        }
-    }
-
     const createButton = (menu, id, text, page) => {
         let item = document.createElement('li');
         menu.appendChild(item);
@@ -75,9 +37,45 @@
         });
     }
 
+    if (uploadPasta) {
+        uploadPasta.addEventListener('submit', event => {
+            const URL = document.getElementById('image-URL');
+            const name = document.getElementById('image-name');
+
+            if (URL.value && name.value) {
+                pasta.post(URL.value, name.value);
+            }
+            URL.value = '';
+            name.value = '';
+
+            event.preventDefault();
+        });
+    }
+
+    const post = (pastasDB, key) => {
+        const data = pastasDB[key];
+        if (myPastaContainer) {
+            return `<div class="img-container">
+                        <a href="${data.imageURL}"><img src="${data.imageURL}" class="pasta-image" /></a>
+                    </div>
+                    <label class="image-name">${data.imageName}</label>
+                    <div class="delete-pasta">
+                        <button class="delete-button" data-id="${key}">Del-Eat Pasta</button>
+                    </div>`;
+        }
+        else {
+            return `<div class="img-container">
+                        <a href="${data.imageURL}"><img src="${data.imageURL}" class="pasta-image" /></a>
+                    </div>                    
+                    <div class="fav-pasta">
+                        <label class="image-name">${data.imageName}</label>
+                    </div>`;
+        }
+    }
+
     const populateContainer = user => {
         if (pastaContainer) {
-            populateUploads();
+            populate();
         }
         else if (myPastaContainer) {
             if (user) {
@@ -89,7 +87,7 @@
         }
         else if (favPastaContainer) {
             if (user) {
-                populateFavourites();
+                populate();
             }
             else {
                 pleaseLogin(favPastaContainer, "favourites");
@@ -97,9 +95,52 @@
         }
     }
 
-    const populateUploads = () => pastasDB.on('child_added', data => {
-        populate(data);
-    });
+    const populate = () => {
+        fetch("https://api.npoint.io/636409df8ded0c89c938").then(response => response.json()).then(db => {
+            const pastasDB = db.pastas;
+            const currentUser = firebase.auth().currentUser;
+
+            hideLoader();
+            for (var key in pastasDB) {
+                if (Object.prototype.hasOwnProperty.call(pastasDB, key)) {
+                    let article = document.createElement('article');
+                    article.classList.add('pasta');
+                    article.innerHTML = post(pastasDB, key);
+
+                    if (currentUser) {
+                        const userId = currentUser.uid;
+
+                        if (!Object.prototype.hasOwnProperty.call(db.users, userId)) {
+                            db.users[userId] = {
+                                "favourites": {},
+                                "pastas": 1
+                            };
+                            database.update(db);
+                        }
+
+                        const dbRef = db.users[userId].favourites;
+                        createFavButton(article, dbRef, key);
+                    }
+
+                    if (pastaContainer) {
+                        pastaContainer.prepend(article);
+                    }
+                    else {
+                        const userId = currentUser.uid;
+                        const dbRef = db.users[userId].favourites;
+
+                        if (dbRef) {
+                            if (Object.prototype.hasOwnProperty.call(dbRef, key)) {
+                                favPastaContainer.prepend(article);
+                            }
+                        }
+                    }
+                }
+            }
+        }).catch((err) => {
+            console.log(err);
+        });
+    }
 
     const populateMyUploads = () => pastasDB.on('child_added', data => {
         hideLoader();
@@ -119,38 +160,6 @@
         }
     });
 
-    const populateFavourites = () => pastasDB.on('child_added', data => {
-        populate(data);
-    });
-
-    const populate = data => {
-        const currentUser = firebase.auth().currentUser;
-
-        hideLoader();
-        let article = document.createElement('article');
-        article.classList.add('pasta');
-        article.innerHTML = post(data);
-
-        if (currentUser) {
-            const userId = currentUser.uid;
-            const dbRef = db.ref('users/' + userId + '/favourites/');
-            createFavButton(article, dbRef, data);
-        }
-
-        if (pastaContainer) {
-            pastaContainer.prepend(article)
-        }
-        else {
-            const userId = currentUser.uid;
-            const dbRef = db.ref('users/' + userId + '/favourites/');
-            dbRef && dbRef.once('value').then(snapshot => {
-                if (snapshot.hasChild(data.key)) {
-                    favPastaContainer.prepend(article);
-                }
-            });
-        }
-    }
-
     const createFavButton = (article, dbRef, data) => {
         let btn = document.createElement('button');
             btn.classList.add("fa-heart");
@@ -161,26 +170,25 @@
 
             article.querySelector('.fav-pasta').append(btn);
 
-            dbRef && dbRef.once('value').then(snapshot => {
-                if (snapshot.hasChild(data.key)) {
+            if (dbRef) {
+                if (Object.prototype.hasOwnProperty.call(dbRef, id)) {
                     btn.classList.add('fas');
                 }
                 else {
                     btn.classList.add('far');
                 }
-            });
+            }
     }
 
-    const favButtonFunctionality = (data, btn) => {
+    const favButtonFunctionality = (id, btn) => {
         btn.addEventListener('click', event => {
-
             if (btn.classList.contains('far')) {
-                pasta.favourite(data.key);
+                pasta.favourite(id);
                 btn.classList.remove('far');
                 btn.classList.add('fas');
             }
             else {
-                pasta.unfavourite(data.key);
+                pasta.unfavourite(id);
                 btn.classList.remove('fas');
                 btn.classList.add('far');
             }
